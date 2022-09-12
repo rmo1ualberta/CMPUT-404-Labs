477c898576f6ac14c7d2ce2db91122caad3b58eb
Ray <rmo1@ualberta.ca>
2022-09-12 17:28:51 -0600

    Updated

--- Lab1/script.py
+++ Lab1/script.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.__version__)
 response = requests.get('http://wwww.google.com/')
-print('The status page of google is ' + response.status_code)
+print(response)
 
 response2 = requests.get('https://raw.githubusercontent.com/rmo1ualberta/CMPUT-404-Labs/master/Lab1/script.py')
 print(response2.content)
