f7fbae4e057f3a0431a6ebbef6b60c865a426a21
Ray <rmo1@ualberta.ca>
2022-09-16 10:59:05 -0600

    Updated

--- Lab2/echo_server.py
+++ Lab2/echo_server.py
@@ -1,19 +1,44 @@
+#!/usr/bin/env python3
 import socket
-import sys
-# based on example code from python docs: https://docs.python.org/3/library/socket.html#socket.socket.connect
-port = int(sys.argv[1])      # port specified in args
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# reuse addresses
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('',port))
-sock.listen(1)
-conn, addr = sock.accept()
-with conn:
-    print('Connected by', addr)
-    while True:
-        data = conn.recv(1024)
-        print(data)
-        if not data: break
-        conn.sendall(data)
+import time
+from multiprocessing import Process
 
-sock.close()
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+        # reuse addr
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        #bind socket to address
+        s.bind((HOST, PORT))
+        #set to listening mode
+        s.listen(2)
+        
+        #continuously listen for connections
+        while True:
+            conn, addr = s.accept()
+            print("Connected by", addr)
+            
+
+            childProcess = Process(target=childRecvAndSend, args=[conn])
+            childProcess.daemon = True
+            childProcess.start()
+
+            conn.close()
+
+def childRecvAndSend(conn):
+
+    #recieve data, wait a bit, then send it back
+    full_data = conn.recv(BUFFER_SIZE)
+    print(full_data)
+    time.sleep(0.5)
+    conn.sendall(full_data)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
